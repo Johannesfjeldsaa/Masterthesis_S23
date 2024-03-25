e7c3a4dc55a6d8561a01b8fed6cf71bfc32d245b
Johannesfjeldsaa <98762880+Johannesfjeldsaa@users.noreply.github.com>
2024-03-25 15:47:38 +0100

    started writing code for GPU training of ML with cuda to speed up learning

---
+++ src/helperfunctions_GPU_ML.py
@@ -0,0 +1,9 @@
+# This file contains helper functions for running classification experiments 
+# author: Johannes Fjeldså
+# email: johannes.larsen.fjeldsa@nmbu.no
+
+# Importing packages
+import cudf 
+import numpy as np
+from tqdm import tqdm
+
